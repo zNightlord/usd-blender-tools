92d661c9a60dfbbd871cf0a18edfc68f141587ff
0znightlord0@gmail.com <0znightlord0@gmail.com>
2023-09-18 14:21:08 +0700

    again again

--- mcskelanim/skel.py
+++ mcskelanim/skel.py
@@ -208,7 +208,7 @@ class UsdRigWrite:
     if self.stage.GetEndTimeCode() < length and extend_stage_end:
       self.stage.SetEndTimeCode(frame)
       
-    anim = UsdSkel.Animation.Define(self.stage, self.skel.GetPath().AppendChild(name))
+    anim = UsdSkel.Animation.Define(self.stage, self.skel.GetPath().AppendChild(f"/{name}"))
     
     translate = {}
     rotate = {}
