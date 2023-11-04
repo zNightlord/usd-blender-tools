e721b476143fb7f44831a498b466cf63f0b11471
0znightlord0@gmail.com <0znightlord0@gmail.com>
2023-11-04 11:07:50 +0700

    split usd_test

--- tests/test_skel.py
+++ tests/test_skel.py
@@ -10,17 +10,20 @@ Path.mkdir(folder, exist_ok=True)
 geom_url = "https://raw.githubusercontent.com/Mojang/bedrock-samples/preview/resource_pack/models/entity/"
 anim_url = "https://raw.githubusercontent.com/Mojang/bedrock-samples/preview/resource_pack/animations/"
 
+
 @pytest.fixture
 def test_base_geom() -> list:
   path = geom_url+"armor_stand.geo.json"
   json = BedrockJSON()
   return json.request_json(path)
 
+
 @pytest.fixture
 def test_base_anim_pose() -> dict:
   path = anim_url+"armor_stand.animation.json"
   json = BedrockJSON()
   return json.request_json(path)
+
 
 @pytest.fixture
 def test_base_skel(test_base_geom: list) -> UsdRigWrite:
@@ -29,15 +32,16 @@ def test_base_skel(test_base_geom: list) -> UsdRigWrite:
   rig.create_stage("rig_output/armor_stand.usda")
   rig.from_json(test_base_geom)
   return rig
-  
+
+
 def test_request(test_base_geom: list):
   assert test_base_geom != None 
+
 
 def test_result(test_base_skel: UsdRigWrite):
   rig = test_base_skel
   # rig.output()
   rig.stage.Save()
-  UsdRigWrite.zip("rig_output/armor_stand.usda", "rig_output/armor_stand.usdz")
   
 
 def test_anim_pose(test_base_skel: UsdRigWrite, test_base_anim_pose: dict):
@@ -45,6 +49,7 @@ def test_anim_pose(test_base_skel: UsdRigWrite, test_base_anim_pose: dict):
   print("\n", "Pose:")
   rig.anim_from_json(test_base_anim_pose, limit=1)
   rig.output()
+
 
 def test_anim_anim():
   path_geo = geom_url+"phantom.geo.json"
@@ -57,10 +62,10 @@ def test_anim_anim():
   rig.create_stage("rig_output/phantom.usda")
   rig.from_json(bones)
   rig.stage.Save()
-  UsdRigWrite.zip("rig_output/phantom.usda", "rig_output/phantom.usdz")
   rig.anim_from_json(anims)
   rig.output()
   print("Hard part.")
+
 
 def test_chicken():
   path_geo = geom_url+"chicken.geo.json"
@@ -73,6 +78,13 @@ def test_chicken():
   rig.create_stage("rig_output/chicken.usda")
   rig.from_json(bones)
   rig.stage.Save()
-  UsdRigWrite.zip("rig_output/chicken.usda", "rig_output/chicken.usdz")
+  
   rig.anim_from_json(anims)
   rig.output()
+
+
+def test_usdz_armorstand():
+  UsdRigWrite.zip("rig_output/armor_stand.usda", "rig_output/armor_stand.usdz")
+  UsdRigWrite.zip("rig_output/chicken.usda", "rig_output/chicken.usdz")
+  UsdRigWrite.zip("rig_output/phantom.usda", "rig_output/phantom.usdz")
+
